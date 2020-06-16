4b48847b522e8ceccaec3f7786b9c4bccf88ad81
MJ <michaljalowik@gmail.com>
2020-06-16 11:24:37 +0200

    first commit

---
+++ src/main/java/Hash/Code/Team/IntercomDemo.java
@@ -0,0 +1,12 @@
+package Hash.Code.Team;
+
+public class IntercomDemo {
+
+    //Intercom intercom = new Intercom();
+    //addExistingInhabitants()
+    //intercom.run();
+
+    void addExistingInhabitants(){
+        // 10 inhabitants to be add
+    }
+}
