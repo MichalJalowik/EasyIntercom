c04f92ec520547026c92ae2b94509cb212414d2d
Lukasz Kaczmarek <lukasz.kaczmarek@vws.vh20.net>
2020-06-17 21:56:58 +0200

    Code reformated

--- src/main/java/Hash/Code/Team/Intercom.java
+++ src/main/java/Hash/Code/Team/Intercom.java
@@ -5,33 +5,33 @@ import java.util.Set;
 
 public class Intercom {
 
-    private Set<Inhabitant> inhabitants = new HashSet<Inhabitant>();
-    
-    public Intercom(Set<Inhabitant> inhabitants) {
-    	this.setInhabitants(inhabitants);
-    }
-    
-    void addInhabitant(Inhabitant inhabitant) {
-        this.inhabitants.add(inhabitant);
-    }
-    
-    boolean open(int flatNumber) {
-        for (Inhabitant inhabitant: this.getInhabitants()) {
-        	if (inhabitant.getFlatNumber() == flatNumber) {
-        		return inhabitant.okForEntry();
-        	}
-        }
-        return false;
-    }
-    
-    boolean open(int flatNumber, int pinCode) {
-        for (Inhabitant inhabitant: this.getInhabitants()) {
-        	if ((inhabitant.getFlatNumber() == flatNumber) && (inhabitant.getPinCode == pinCode)) {
-        		return true;
-        	}
-        }
-        return false;
-    }
+	private Set<Inhabitant> inhabitants = new HashSet<Inhabitant>();
+
+	public Intercom(Set<Inhabitant> inhabitants) {
+		this.setInhabitants(inhabitants);
+	}
+
+	void addInhabitant(Inhabitant inhabitant) {
+		this.inhabitants.add(inhabitant);
+	}
+
+	boolean open(int flatNumber) {
+		for (Inhabitant inhabitant : this.getInhabitants()) {
+			if (inhabitant.getFlatNumber() == flatNumber) {
+				return inhabitant.okForEntry();
+			}
+		}
+		return false;
+	}
+
+	boolean open(int flatNumber, int pinCode) {
+		for (Inhabitant inhabitant : this.getInhabitants()) {
+			if ((inhabitant.getFlatNumber() == flatNumber) && (inhabitant.getPinCode == pinCode)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public Set<Inhabitant> getInhabitants() {
 		return inhabitants;
