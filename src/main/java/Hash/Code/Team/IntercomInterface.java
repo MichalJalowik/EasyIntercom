d00a6549be644c0624f65a21e26fb5763883e878
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-18 07:52:26 +0200

    scanInt method implemented

--- src/main/java/Hash/Code/Team/IntercomInterface.java
+++ src/main/java/Hash/Code/Team/IntercomInterface.java
@@ -1,14 +1,42 @@
 package Hash.Code.Team;
 
+import java.util.Scanner;
+
 public class IntercomInterface {
 
-    Intercom intercom;
+    private Intercom intercom;
+    private Scanner scanner;
 
     public IntercomInterface(Intercom intercom) {
         this.intercom = intercom;
+        scanner = new Scanner(System.in);
     }
 
-    void startInterface() {
-        //TODO
+    public void startInterface() {
+        boolean inLoop = true;
+
+        while (inLoop) {
+            System.out.println("Please");
+
+
+        }
+    }
+
+    private int scanInt(String instruction) {
+        int input = 0;
+        boolean inLoop = true;
+
+        while (inLoop) {
+            try {
+                System.out.println(instruction);
+                input = scanner.nextInt();
+                scanner.nextLine();
+            }
+            catch (Exception e) {
+                System.out.println("Wrong input!\n");
+                scanner.nextLine();
+            }
+        }
+        return input;
     }
 }
