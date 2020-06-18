de8871c6f9aa1a4d9b8ab711ab89a8b20978ffef
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-18 09:05:25 +0200

    Working code

--- src/main/java/Hash/Code/Team/IntercomInterface.java
+++ src/main/java/Hash/Code/Team/IntercomInterface.java
@@ -16,9 +16,7 @@ public class IntercomInterface {
         boolean inLoop = true;
 
         while (inLoop) {
-            System.out.println("Please");
-
-
+            inLoop = operateNumberTyping();
         }
     }
 
@@ -28,15 +26,65 @@ public class IntercomInterface {
 
         while (inLoop) {
             try {
-                System.out.println(instruction);
+                System.out.println("\n" + instruction);
                 input = scanner.nextInt();
                 scanner.nextLine();
+                break;
             }
             catch (Exception e) {
-                System.out.println("Wrong input!\n");
+                System.out.println("\nWrong input!");
                 scanner.nextLine();
             }
         }
         return input;
+    }
+
+    private boolean operateNumberTyping() {
+        int flatNumber = scanInt("Please type flat number:");
+        boolean isNumberValid = isNumberValid(flatNumber);
+
+        if (flatNumber == 9999) {
+            System.out.println("\nIntercom turned off...");
+            return false;
+        } else if (isNumberValid) {
+            int pinCode = scanInt("Please type PIN code or type '0' to connect with the flat:");
+
+            if (pinCode == 0) {
+                System.out.println("\nIntercom is connecting with flat " + flatNumber + "...");
+                wait(2000);
+                System.out.println("\n" + intercomResponse(intercom.open(flatNumber)));
+            } else {
+                wait(1000);
+                System.out.println("\n" + intercomResponse(intercom.open(flatNumber, pinCode)));
+            }
+            return true;
+        } else {
+            System.out.println("\nFlat " + flatNumber + " not found...");
+            return true;
+        }
+    }
+
+    private boolean isNumberValid(int flatNumber) {
+        for (Inhabitant inhabitant : intercom.getInhabitants()) {
+            if (inhabitant.getFlatNumber() == flatNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String intercomResponse(boolean isOpen) {
+        if (isOpen) {
+            return "Door has been opened.";
+        }
+        return "Nothing happened...";
+    }
+
+    private void wait(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
