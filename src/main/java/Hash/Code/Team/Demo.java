95eeacc42072af1accd4cf134c1b9d56371ac401
MJ <michaljalowik@gmail.com>
2020-06-18 20:30:58 +0200

    finished after sprint #1 ! :)

--- src/main/java/Hash/Code/Team/Demo.java
+++ src/main/java/Hash/Code/Team/Demo.java
@@ -1,16 +1,29 @@
 package Hash.Code.Team;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Demo {
 
     IntercomInterface simpleInterface;
     
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
+        Inhabitant i1 = new Inhabitant(111, 6666);
+        Inhabitant i2 = new Inhabitant(222, 7777);
+        Inhabitant i3 = new Inhabitant(333, 8888);
+        Set<Inhabitant> inhabitants = new HashSet<Inhabitant>();
+        inhabitants.add(i1);
+        inhabitants.add(i2);
+        inhabitants.add(i3);
+
+        Intercom intercom = new Intercom(inhabitants);
+        IntercomInterface intercomInterface = new IntercomInterface(intercom);
+        intercomInterface.startInterface();
     }
     
     void addDemoInhabitants() {
-        //TODO
+
     }
 
 }
