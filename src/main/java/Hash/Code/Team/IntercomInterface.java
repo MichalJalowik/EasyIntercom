7454723aebbb0d3d4ece462fd788d069d46e82f1
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-06-18 07:38:22 +0200

    Constructor added

--- src/main/java/Hash/Code/Team/IntercomInterface.java
+++ src/main/java/Hash/Code/Team/IntercomInterface.java
@@ -3,7 +3,11 @@ package Hash.Code.Team;
 public class IntercomInterface {
 
     Intercom intercom;
-    
+
+    public IntercomInterface(Intercom intercom) {
+        this.intercom = intercom;
+    }
+
     void startInterface() {
         //TODO
     }
